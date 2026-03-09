4a548e894c7b2755e87492931192c88125ea89fc
Ben Schwarz <23khwq@queensu.ca>
2026-03-09 15:32:52 -0400

    File processing script added

---
+++ data_storage.py
@@ -0,0 +1,23 @@
+import h5py
+import numpy as np
+
+# Name will be as follows: "type_" + dataset_name -> e.g. "train_walking_ben", "test_walking_ben", "raw_walking_ben"
+dataset_name = "walking_ben"
+data = np.loadtxt('Data_WalkingOutside_Ben.xlsx', delimiter=',', skiprows=1)
+
+window_len = 500
+num_windows = len(data) // window_len
+truncated_data = data[:num_windows * window_len]
+windows = truncated_data.reshape(-1, window_len, 3)
+
+split = int(0.9 * num_windows)
+train_set = windows[:split]
+test_set = windows[split:]
+
+with h5py.File('h5py_data.h5', 'w') as f:
+    processed = f.create_gropup('processed')
+    raw = f.create_group('raw')
+
+    processed.create_dataset("train_" + dataset_name, data=train_set, compression='gzip')
+    processed.create_dataset("test_" + dataset_name, data=test_set, compression='gzip')
+    raw.create_dataset("raw_" + dataset_name, data=truncated_data, compression='gzip')
